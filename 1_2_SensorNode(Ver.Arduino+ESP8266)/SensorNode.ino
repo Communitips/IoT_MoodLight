548ad96ed11be90b3f490dce9a5c028808293f1a
david712 <david712@outlook.kr>
2017-09-27 01:53:05 +0900

    디렉토리 정리 및 센서노드 아두이노 소스 커밋

---
+++ 1_2_SensorNode(Ver.Arduino+ESP8266)/SensorNode.ino
@@ -0,0 +1,189 @@
+#include <SoftwareSerial.h>
+#include <DHT11.h>
+#include "A_ESP8266.h"
+
+#define PWM_LED_PIN 9
+#define DHT_PIN 12
+#define ESP_TX  10
+#define ESP_RX  11
+
+#define SSID        "DKWS"
+#define PASSWORD    "110258502241"
+#define HOST_ADDR   "192.168.1.10"
+#define HOST_PORT   (25602)
+
+int err;
+DHT11 dht11(DHT_PIN);
+float   dht_temp, dht_humi;
+
+SoftwareSerial esp8266(ESP_RX, ESP_TX); //RX 11, TX 10
+A_ESP8266 wifi(esp8266);
+
+unsigned char buffer[256];
+
+uint32_t TCP_Receive(void)
+{
+  uint32_t len = wifi.recv(buffer, sizeof(buffer), 1000);
+  if (len > 0)  return 1;
+  else          return 0;
+}
+
+void TCP_Send(unsigned char key, unsigned char msg_id, unsigned char body_size, unsigned char* body)
+{
+  int i;
+  unsigned char send_buf[256];
+  memset(send_buf, 0x00, sizeof(send_buf));
+
+  *send_buf = key;
+  *(send_buf+1) = msg_id;     //msg_id (temperature 0x01, humidity 0x02, light 0x04, switch 0x10)
+  *(send_buf+2) = body_size;
+
+  memcpy((send_buf+3), body, body_size);
+
+  wifi.send(send_buf, body_size + 3);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode( PWM_LED_PIN, OUTPUT );
+  analogWrite( PWM_LED_PIN, 0);
+}
+
+int ConnectStep = 1;
+void loop() {
+  #if 1
+  switch(ConnectStep)
+  {
+  case 1:
+    if(wifi.setOprToStationSoftAP())
+    {
+      Serial.print("[1] to station + softap ok\r\n");
+      ConnectStep++;
+    }
+    break;
+
+  case 2:
+    if (wifi.joinAP(SSID, PASSWORD))
+    {
+      Serial.print("[2] Join AP success\r\n");
+      Serial.println( wifi.getLocalIP().c_str());
+      ConnectStep++;
+    }
+    break;
+
+  case 3:
+    if (wifi.disableMUX())
+    {
+      Serial.print("[3] single ok\r\n");
+      ConnectStep++;
+    }
+    break;
+
+  case 4:
+    if (wifi.createTCP(HOST_ADDR, HOST_PORT))
+    {
+      Serial.print("[4] create tcp ok\r\n");
+      DataNode_Main();
+    }
+    break;
+  }
+  delay(500);
+#else
+  DataNode_Main();
+#endif
+}
+
+void MakeFormat(unsigned char format, void* data, unsigned char* buf)
+{
+  float tmp_f;
+  if(format & 0x01)
+  {
+    tmp_f = *(float*)data;
+    buf[0] = (unsigned char)((int)tmp_f/10);
+    buf[1] = (unsigned char)((int)tmp_f%10);
+    tmp_f = (tmp_f - (int)tmp_f)*100;
+    buf[2] = (unsigned char)((int)tmp_f/10);
+    buf[3] = (unsigned char)((int)tmp_f%10);
+  }
+}
+
+bool isLedOn = true;
+
+void Sense_Proc(unsigned char mode)
+{
+  int i;
+  if(err = dht11.read(dht_humi, dht_temp) == 0)
+  {
+    MakeFormat(0x01, (void*)&dht_temp, &buffer[3]);
+    MakeFormat(0x01, (void*)&dht_humi, &buffer[7]);
+    
+    //send packet
+    TCP_Send(27, 0x03, 8, &buffer[3]);
+  }
+  else
+  {
+    ;//send error packet
+  }
+
+  //if(mode)    delay(DHT11_RETRY_DELAY);
+}
+
+void Light_Proc(void)
+{
+  //toggle
+  if(isLedOn)    isLedOn = false;
+  else           isLedOn = true;
+
+  //setting
+  // - delay
+  // - brightness
+}
+
+void DataNode_Main()
+{
+  int i;
+  
+  while(1)
+  {
+    //========GET COMMAND========//
+    if(TCP_Receive())
+    {
+      if(buffer[0] == 27)
+      {
+        if(buffer[1] & 0x01) {
+          Sense_Proc(0);
+        }
+        
+        if(buffer[1] & 0x02) {
+          Light_Proc();
+        }
+        
+        //...
+      }
+    }
+    
+    //=======Sense Proc==========//
+    Sense_Proc(1);
+    
+    //=======Light Proc==========//
+    if(isLedOn)
+    {
+      for (i = 20; i < 256; i++)
+      {
+        analogWrite(PWM_LED_PIN, i);
+        delay(10);
+      }
+      for (i = 255; i >= 20; i--)
+      {
+        analogWrite(PWM_LED_PIN, i);
+        delay(10);
+      }
+    }
+    else
+    {
+      analogWrite(PWM_LED_PIN, 0);
+      delay(5000);
+    }
+  }  
+}
+
